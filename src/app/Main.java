cc07114c5fff215073d8c70cfd78813ed8adc369
B00361078 <B00361078@studentmail.uws.ac.uk>
2021-09-27 22:19:31 +0100

    initial commit

---
+++ src/app/Main.java
@@ -0,0 +1,31 @@
+package app;
+	
+import java.sql.PreparedStatement;
+import javafx.application.Application;
+import javafx.stage.Stage;
+import weka.core.converters.ArffLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.fxml.FXMLLoader;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			PreparedStatement prepared_statement = null;
+			ArffLoader arf = new ArffLoader();
+			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Sample.fxml"));
+			Scene scene = new Scene(root,400,400);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
