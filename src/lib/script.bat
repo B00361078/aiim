f3e2ac58d5ed383a3a013b1a9d27fc8b0220be9e
B00361078 <B00361078@studentmail.uws.ac.uk>
2021-10-04 12:55:22 +0100

    update

--- src/lib/script.bat
+++
@@ -1,4 +0,0 @@
-echo "executing powershell to download weka library jar"
-Powershell.exe -ExecutionPolicy Bypass -File "weka.ps1"
-echo "installing weka jar as dependency in m2 local repo"
-mvn install:install-file -Dfile="C:\Program Files\Weka-3-8-5\weka.jar" -DgroupId=weka -DartifactId=weka -Dversion=3.8.5 -DgeneratePom=true -Dpackaging=jar
--- src/module-info.java
+++
@@ -1,8 +0,0 @@
-module aiim {
-	exports app;
-	requires javafx.fxml;
-	requires javafx.controls;
-	requires javafx.graphics;
-	requires weka;
-	requires java.sql;
-}
