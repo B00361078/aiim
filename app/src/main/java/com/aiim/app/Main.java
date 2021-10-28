87224c0fd83300124c06bea35d54363f7020c786
B00361078 <B00361078@studentmail.uws.ac.uk>
2021-10-28 13:37:17 +0100

    updated

--- app/src/main/java/com/aiim/app/Main.java
+++ app/src/main/java/com/aiim/app/Main.java
@@ -2,8 +2,17 @@ package com.aiim.app;
 	
 import javafx.application.Application;
 import javafx.stage.Stage;
-
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.sql.Blob;
 import java.sql.Connection;
+import java.sql.Date;
 
 import org.deeplearning4j.iterator.CnnSentenceDataSetIterator;
 
@@ -26,13 +35,101 @@ public class Main extends Application {
 		viewController = new ViewController();
 		viewController.setCurrentStage(stage);
 		viewController.switchToView(ViewNames.LOGIN);
-		//con = DatabaseConnect.getConnection();
+		con = DatabaseConnect.getConnection();
+		//download();
+		update();
+		//upload();
 		//WordVector wv = new WordVector();
 		//wv.generateVectors();
-		MyCnn2.cnn();
+		//MyCnn2.cnn();
 		//CnnSentenceClassificationExample.cnn();
 		//System.out.println(con);
 		
     }
+	public void upload () throws Exception {
+
+		File file;
+	    file = new File("trained_model2.zip");// ...(file is initialised)...
+	    byte[] fileContent = Files.readAllBytes(file.toPath());
+	    String filename = file.getName();
+	    String mode = "testmode";
+	    long filelength = file.length();
+	    long filelengthinkb = filelength/1024;
+	    PreparedStatement prepared_statement = null;
+	    java.util.Date date = new java.util.Date();
+	    Object param = new java.sql.Timestamp(date.getTime());
+	    // The JDBC driver knows what to do with a java.sql type:
+	    con.setAutoCommit(false);
+	    prepared_statement = con.prepareStatement("USE [honsdb] INSERT INTO tblClassifier (fileName,size,extension,fileContent,dateGenerated,mode) VALUES(?,?,?,?,?,?)");
+	    
+	    		prepared_statement.setString(1, filename);
+	    		prepared_statement.setLong(2, filelengthinkb);
+	    		prepared_statement.setString(3, filename.substring(filename.lastIndexOf(".")+1));
+	    		prepared_statement.setBytes(4, fileContent);
+	    		prepared_statement.setObject(5, param);
+	    		prepared_statement.setString(6, mode);
+	    		
+	    		
+	    		if (prepared_statement.executeUpdate() == 1)
+	    		{
+	    			con.commit();
+	    			System.out.println("Byte Array Stored Successfully in SQL Server");
+	    		}
+	    		else
+	    		{
+	    			throw new Exception("Problem occured during Save");
+	    		}
+	    
+	}
+	public void download() throws IOException, SQLException {
+		PreparedStatement prepared_statement2 = con.prepareStatement("USE [honsdb] SELECT *FROM tblClassifier");
+		ResultSet rs = prepared_statement2.executeQuery();
+		
+		while (rs.next()) {
+			int fileID = rs.getInt("id");
+			String filename2 = "temp_" + rs.getString("fileName");
+			long filesizeinkb2 = rs.getLong("size");
+			String ext = rs.getString("extension");
+			Blob blob = rs.getBlob("fileContent");
+			System.out.println(fileID);
+			System.out.println(filename2);
+			System.out.println(filesizeinkb2);
+			System.out.println(ext);
+			InputStream inputstr = blob.getBinaryStream();
+			Files.copy(inputstr, Paths.get(filename2));
+			
+		}
+		
+	}
+	public void update() throws SQLException, IOException {
+		File file;
+	    file = new File("trained_model.zip");// ...(file is initialised)...
+	    byte[] fileContent = Files.readAllBytes(file.toPath());
+	    String filename = file.getName();
+	    String mode = "testmode";
+	    long filelength = file.length();
+	    long filelengthinkb = filelength/1024;
+	    con.setAutoCommit(false);
+	    //update db
+		//PreparedStatement prepared_statement3 = con.prepareStatement("USE [honsdb] UPDATE tblClassifier SET size=?, fileContent=?, mode=? WHERE id=1");
+		PreparedStatement prepared_statement3 = con.prepareStatement("USE [honsdb] UPDATE tblClassifier SET mode=? WHERE id=4");
+		//prepared_statement3.setLong(1, filelengthinkb);
+		//prepared_statement3.setBytes(2, fileContent);
+		prepared_statement3.setString(1, "semi");
+		
+		if (prepared_statement3.executeUpdate() == 1)
+		{
+			con.commit();
+			System.out.println("Updated successfully");
+		}
+		else
+		{
+			System.out.println("Problem occured during update");
+		}
+		
+		//prepared_statement3.executeUpdate();
+		//System.out.println("updated now");
+		
+	}
 	
 }
