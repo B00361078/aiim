9410e4b0efa5a1811f48b948f8897e4e466abee1
B00361078 <B00361078@studentmail.uws.ac.uk>
2022-01-03 14:51:17 +0000

    ai percentage

--- app/src/main/java/com/aiim/app/controller/SettingController.java
+++ app/src/main/java/com/aiim/app/controller/SettingController.java
@@ -3,6 +3,8 @@ package com.aiim.app.controller;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 import com.aiim.app.database.DatabaseConnect;
 import com.aiim.app.resource.ViewNames;
@@ -12,6 +14,7 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
+import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.ToggleGroup;
 
@@ -30,9 +33,14 @@ public class SettingController {
 	@FXML private RadioButton assignOff;
 	@FXML private RadioButton trainOn;
 	@FXML private RadioButton trainOff;
+	@FXML private Label percentage;
 	private AppUtil appUtil;
 	private Alert alert;
 	private ResourceBundle strBundle;
+	private ResultSet rs;
+	private int totalClosed;
+	private int totalAssignedCorrect;
+	private float accuracy;
    
 	public void initialize() throws Exception {
 		strBundle = ResourceBundle.getBundle("com.aiim.app.resource.bundle");
@@ -50,6 +58,7 @@ public class SettingController {
     	setAction(assignOff, "Are you sure you want to disable Auto-assign mode?", "OFF", "Disabled", "Auto-assign");
     	setAction(trainOn, "Are you sure you want to enable Auto-train mode?", "ON", "Enabled", "Auto-train");
     	setAction(trainOff, "Are you sure you want to disable Auto-train mode?", "OFF", "Disabled", "Auto-train");
+    	getPercentage();
     }
     
     public void setAction (RadioButton button, String message, String mode, String req, String modeName) {
@@ -69,6 +78,7 @@ public class SettingController {
             	}
             });
     }
+    
     public void setRadio(String mode, String modeVal) throws Exception {
     	switch(mode) {
     	case "assignMode":
@@ -115,5 +125,20 @@ public class SettingController {
     
     @FXML protected void back() throws IOException {
     	ViewController.createInstance().switchToView(ViewNames.DASHBOARD);
-    }  
+    }
+    
+    private void getPercentage() throws SQLException {
+    	sqlStatement = con.prepareStatement(strBundle.getString("sqlSelect10"));
+    	rs = sqlStatement.executeQuery();
+	    	while(rs.next()){
+	    		totalClosed = rs.getInt(1);
+	    	}
+    	sqlStatement = con.prepareStatement(strBundle.getString("sqlSelect11"));
+    	rs = sqlStatement.executeQuery();
+	    	while(rs.next()){
+	    		totalAssignedCorrect = rs.getInt(1);
+	    	}
+    	accuracy = (totalAssignedCorrect * 100.0f) / totalClosed;
+    	percentage.setText(String.format("%.2f", accuracy));
+    }
 }
